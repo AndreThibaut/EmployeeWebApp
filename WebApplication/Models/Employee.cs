33afe91f7deee4a7b2d3af10d7bfd4ebd7839654
AndreThibaut <and.thibaut@gmail.com>
2019-09-18 16:35:55 +0200

    Add project files.

---
+++ WebApplication/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        [Column(TypeName ="nvarchar(100)")]
+        public string FirstName { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string EmployeeCode { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string Position { get; set; }
+
+    }
+}
